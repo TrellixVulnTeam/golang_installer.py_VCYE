367531f787ab8fa4fa5b1b529e8c5c137bbac056
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 16:16:14 +0000

    Adding tarfile member sanitization to extractall()

--- golang_installer.py
+++ golang_installer.py
@@ -227,7 +227,26 @@ def download_and_install(response_json, version, prefix, force):
         print("SHA256 OK")
         print("Extracting...")
         with tarfile.open(fileobj=temp_file, mode="r:gz") as tar:
-            tar.extractall(prefix, members=members(tar))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, prefix, members=members(tar))
         print("Done extracting")
 
     print("Setting up environment...")
